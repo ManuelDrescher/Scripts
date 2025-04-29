1496c9597debcb9d3dff0dd8ca67b0e674512dff
Manu <209072369+ManuelDrescher@users.noreply.github.com>
2025-04-29 14:19:25 +0200

    Update pve-update_trim_clean.sh

--- ProxmoxVE/pve-update_trim_clean.sh
+++ ProxmoxVE/pve-update_trim_clean.sh
@@ -2,22 +2,22 @@
 sleep 1
 echo Updating LXC:
 read -p "Press Enter to continue...."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/drescher-manu/Scripts/main/ProxmoxVE/tools/pve/update-lxcs.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/manueldrescher/Scripts/main/ProxmoxVE/tools/pve/update-lxcs.sh)"
 sleep 1
 echo Cleaning LXC:
 echo apt-get --purge autoremove + autoclean
 echo + deleting content of /var/lib/apt/lists/ + updating apt package list
 echo
 read -p "Press Enter to continue...."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/drescher-manu/Scripts/main/ProxmoxVE/tools/pve/clean-lxcs.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/manueldrescher/Scripts/main/ProxmoxVE/tools/pve/clean-lxcs.sh)"
 sleep 1
 echo Trimming Filesystem on LXC and the executed Node:
 read -p "Press Enter to continue...."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/drescher-manu/Scripts/main/ProxmoxVE/tools/pve/fstrim.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/manueldrescher/Scripts/main/ProxmoxVE/tools/pve/fstrim.sh)"
 sleep 1
 echo Removing old and unused Kernels on executed Node:
 read -p "Press Enter to continue...."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/drescher-manu/Scripts/main/ProxmoxVE/tools/pve/kernel-clean.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/manueldrescher/Scripts/main/ProxmoxVE/tools/pve/kernel-clean.sh)"
 sleep 1
 echo Finished!
 read -p "Press Enter to exit...."
