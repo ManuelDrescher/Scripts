40fdf9d668782a7f760c3573a9547bee9fc46816
Manuel <209072369+drescher-manu@users.noreply.github.com>
2025-04-29 12:56:50 +0200

    Create ad_rejoin.sh

---
+++ giDcvS/AD_rejoin/ad_rejoin.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+###################################################################
+## AD Rejoin                                                     ##
+##                                                               ##
+## Autor: XXX                                                    ##
+## Datum: XXX                                                    ##
+##                                                               ##
+## Inhalt: Vollständige Rejoin in die Domäne <DOMAIN>            ##
+##                                                               ##
+## Zentrale Ablage, Aufruf erfolgt Remote über patch_me.sh       ##
+###################################################################
+
+#Shell-Verhalten definieren
+set -eEuo pipefail
+#Anzeige des grafischen Auswahlmenue
+whiptail --backtitle "AD Rejoin" --title "" --yesno "Das System wird erneut in die AD aufgenommen. Fortfahren?" 10 58
+if [ $? -ne 0 ]; then
+  exit
+fi
+clear
+sleep 1
+echo
+#Anzeigen des Hostname
+echo Hostname ist:
+echo $HOSTNAME
+echo
+sleep 1
+echo Rejoin in die AD beginnt....
+sleep 1
+#Durchfuehren AD Rejoin
+sudo rm /etc/krb5.keytab
+sudo rm -rf /var/lib/sss/db/*
+sudo rm -f /tmp/krb5*
+sudo cp /etc/sssd/sssd.conf /etc/sssd/sssd.bak
+sudo realm leave
+echo Bitte <TIER-ACCOUNT> OHNE @<DOMAIN> eingeben
+echo Accountpasswort des <TIER-ACCOUNT> wird im Anschluss abgefragt
+echo
+sleep 1
+read -p "Bitte <TIER-ACCOUNT> eingeben (bspw. <TIER>MaxMustermann): " DomainAdmin
+sudo realm join -U $DomainAdmin <DOMAIN>
+sudo mv /etc/sssd/sssd.bak /etc/sssd/sssd.conf
+sudo systemctl restart sssd
+sudo systemctl status sssd
